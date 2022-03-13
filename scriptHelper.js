b48b672dfdd8498a10904129e4eda5021929381a
Sami Freese <sami@Samis-MacBook-Air.local>
2022-03-12 18:48:49 -0600

    Input Validation complete

--- scriptHelper.js
+++ scriptHelper.js
@@ -32,10 +32,36 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let copilotValidation = validateInput(copilot);
     let fuelValidation = validateInput(fuelLevel);
     let cargoValidation = validateInput(cargoLevel);
+
+    document.querySelector("#pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`
+    document.querySelector("#copilotStatus").innerHTML = `Co-Pilot ${copilot} is ready for launch`
+
     if ([pilotValidation, copilotValidation, fuelValidation, cargoValidation].includes("Empty")) {
         alert("All fields are required.");
     } else if ([pilotValidation, copilotValidation].includes("Is a Number") || [fuelValidation, cargoValidation].includes("Not a Number")) {
         alert("Please enter valid information for each field.")
+    } else if (fuelLevel < 10000 && cargoLevel > 10000) {
+        document.querySelector("#launchStatus").innerHTML = "Shuttle not ready for launch"
+        document.querySelector("#launchStatus").style.color = "red"
+        list.style.visibility = "visible"
+        document.querySelector("#fuelStatus").innerHTML = "Fuel level too low for launch"
+        document.querySelector("#cargoStatus").innerHTML = "Cargo mass too high for launch"
+    } else if (fuelLevel < 10000 ) {
+        document.querySelector("#launchStatus").innerHTML = "Shuttle not ready for launch"
+        document.querySelector("#launchStatus").style.color = "red"
+        list.style.visibility = "visible"
+        document.querySelector("#fuelStatus").innerHTML = "Fuel level too low for launch"
+        document.querySelector("#cargoStatus").innerHTML = "Cargo mass low enough for launch"
+    } else if (cargoLevel > 10000) {
+        document.querySelector("#launchStatus").innerHTML = "Shuttle not ready for launch"
+        document.querySelector("#launchStatus").style.color = "red"
+        list.style.visibility = "visible"
+        document.querySelector("#fuelStatus").innerHTML = "Fuel level high enough for launch"
+        document.querySelector("#cargoStatus").innerHTML = "Cargo mass too high for launch"
+    } else {
+        document.querySelector("#launchStatus").innerHTML = "Shuttle is ready for launch"
+        document.querySelector("#launchStatus").style.color = "green"
+        list.style.visibility = "hidden"
     }
 }
 
